c789208a080caa7a03ab437e79fe828f1a18e08c
Hakan Karaotcu <hakankaraotcu@gmail.com>
2022-11-09 11:56:50 +0300

    added 'news' page

--- app/src/main/java/com/hakankaraotcu/gamedb/JournalFragment.java
+++ app/src/main/java/com/hakankaraotcu/gamedb/JournalFragment.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class JournalFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private JournalsAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,5 +27,13 @@ public class JournalFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.journal_recyclerView);
+        adapter = new JournalsAdapter(Journals.getData(), getContext());
+
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/hakankaraotcu/gamedb/Journals.java
@@ -0,0 +1,59 @@
+package com.hakankaraotcu.gamedb;
+
+import java.util.ArrayList;
+
+public class Journals {
+    private String journalTitle, journalContent;
+    private int journalImage;
+
+    public Journals(){
+
+    }
+
+    public Journals(String journalTitle, String journalContent){
+        this.journalTitle = journalTitle;
+        this.journalContent = journalContent;
+    }
+
+    public String getJournalTitle() {
+        return journalTitle;
+    }
+
+    public void setJournalTitle(String journalTitle) {
+        this.journalTitle = journalTitle;
+    }
+
+    public String getJournalContent() {
+        return journalContent;
+    }
+
+    public void setJournalContent(String journalContent) {
+        this.journalContent = journalContent;
+    }
+
+    public int getJournalImage() {
+        return journalImage;
+    }
+
+    public void setJournalImage(int journalImage) {
+        this.journalImage = journalImage;
+    }
+
+    static public ArrayList<Journals> getData(){
+        ArrayList<Journals> journalsList = new ArrayList<Journals>();
+        String[] titles = {"Journal1", "Journal2", "Journal3", "Journal4", "Journal5", "Journal6", "Journal7"};
+        String[] contents = {"Overwatch 2 Announces New Tank Hero, Ramattra", "Overwatch 2 Announces New Tank Hero, Ramattra", "Overwatch 2 Announces New Tank Hero, Ramattra", "Overwatch 2 Announces New Tank Hero, Ramattra", "Overwatch 2 Announces New Tank Hero, Ramattra", "Overwatch 2 Announces New Tank Hero, Ramattra", "Overwatch 2 Announces New Tank Hero, Ramattra"};
+        int[] images = {R.drawable.journal, R.drawable.journal, R.drawable.journal, R.drawable.journal, R.drawable.journal, R.drawable.journal, R.drawable.journal,};
+
+        for(int i = 0;i < titles.length;i++){
+            Journals journal = new Journals();
+            journal.setJournalTitle(titles[i]);
+            journal.setJournalContent(contents[i]);
+            journal.setJournalImage(images[i]);
+
+            journalsList.add((journal));
+        }
+
+        return journalsList;
+    }
+}
