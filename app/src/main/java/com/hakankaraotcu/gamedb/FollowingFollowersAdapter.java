9d5527adb254a31762419015b232df0c2bbf0801
Hakan Karaotcu <hakankaraotcu@gmail.com>
2022-12-11 22:01:16 +0300

    added 'following' page

---
+++ app/src/main/java/com/hakankaraotcu/gamedb/FollowingFollowersAdapter.java
@@ -0,0 +1,43 @@
+package com.hakankaraotcu.gamedb;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+public class FollowingFollowersAdapter extends ArrayAdapter<String> {
+    private int[] images;
+    private String[] usernameList;
+    private Context context;
+    private CircularImageView image;
+    private TextView username;
+
+    public FollowingFollowersAdapter(String[] usernameList, int[] images, Context context) {
+        super(context, R.layout.following_followers, usernameList);
+        this.usernameList = usernameList;
+        this.images = images;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.following_followers, null);
+
+        if (view != null) {
+            image = view.findViewById(R.id.follower_image);
+            username = view.findViewById(R.id.follower_username);
+
+            image.setImageResource(images[position]);
+            username.setText(usernameList[position]);
+        }
+        return view;
+    }
+}
--- app/src/main/java/com/hakankaraotcu/gamedb/UserFollowingFragment.java
+++ app/src/main/java/com/hakankaraotcu/gamedb/UserFollowingFragment.java
@@ -9,18 +9,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 public class UserFollowingFragment extends Fragment {
+    private ListView listView;
+    private FollowingFollowersAdapter adapter;
+    private int[] images = {R.drawable.discoelysium, R.drawable.monsterhunter, R.drawable.halflife, R.drawable.cuphead, R.drawable.darksiders2};
+    private String[] usernames = {"John", "Mike", "Kelly", "Ashe", "Joke"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user_following, container, false);
+        listView = view.findViewById(R.id.userFollowing_listView);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        adapter = new FollowingFollowersAdapter(usernames, images, getContext());
+        listView.setAdapter(adapter);
     }
 }
